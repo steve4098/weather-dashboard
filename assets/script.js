f095dc518c11eca5caeedece55875d31ab78afa5
steve4098 <139075699+steve4098@users.noreply.github.com>
2023-09-15 18:54:46 +0100

    element added to style.css for search button. variable created in script.js for search history

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,16 @@ let APIKey = "366d8630062f3d02a6b4a52f0ab43452";
 
 let queryURL = "https://api.openweathermap.org/data/3.0/onecall/day_summary?lat=" + lat + "&lon=" + lon + "&date=" + date + "&appid=" + APIKey
 
+//search history//
+let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+
 let lat = ();
 let lon = ();
 let date = ();
+
+//function to create saved search list//
+
+function renderButtons() {
+    $("#history").empty();
+
+}
