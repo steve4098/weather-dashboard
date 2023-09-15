be51e293751a9cc41bfda67c1f595c25223035be
steve4098 <139075699+steve4098@users.noreply.github.com>
2023-09-15 19:18:58 +0100

    function created for submitting the search form

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,17 @@ let APIKey = "366d8630062f3d02a6b4a52f0ab43452";
 
 let queryURL = "https://api.openweathermap.org/data/3.0/onecall/day_summary?lat=" + lat + "&lon=" + lon + "&date=" + date + "&appid=" + APIKey
 
-//serach bar//
+//search bar//
 let searchInput = $("#search-input");
 let searchForm = $("#search-form");
+
+function submitSearchForm(event) {
+    event.preventDefault();
+
+    let search = searchInput.val().trim();
+}
+
+//
 
 let lat = ();
 let lon = ();
