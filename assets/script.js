d6b8dee39bf604d906fc712c8e7b0dcdc27dc6aa
steve4098 <139075699+steve4098@users.noreply.github.com>
2023-09-15 18:46:58 +0100

    variables created ready to hold lat, lon and date

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,6 @@ let APIKey = "366d8630062f3d02a6b4a52f0ab43452";
 
 let queryURL = "https://api.openweathermap.org/data/3.0/onecall/day_summary?lat=" + lat + "&lon=" + lon + "&date=" + date + "&appid=" + APIKey
 
+let lat = ();
+let lon = ();
+let date = ();
