f787d43445072b5a72efd6bb09fd2478ccc60c0c
steve4098 <139075699+steve4098@users.noreply.github.com>
2023-09-15 19:13:14 +0100

    created variables for search input and search form by ID

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,10 @@ let APIKey = "366d8630062f3d02a6b4a52f0ab43452";
 
 let queryURL = "https://api.openweathermap.org/data/3.0/onecall/day_summary?lat=" + lat + "&lon=" + lon + "&date=" + date + "&appid=" + APIKey
 
+//serach bar//
+let searchInput = $("#search-input");
+let searchForm = $("#search-form");
+
 //search history//
 let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 
