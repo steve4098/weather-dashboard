2301beed783d9a9497d55b9ddb89e9e04702704d
steve4098 <139075699+steve4098@users.noreply.github.com>
2023-09-15 18:44:58 +0100

    variable created to hold APIkey and queryURL

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+let APIKey = "366d8630062f3d02a6b4a52f0ab43452";
+
+let queryURL = "https://api.openweathermap.org/data/3.0/onecall/day_summary?lat=" + lat + "&lon=" + lon + "&date=" + date + "&appid=" + APIKey
+
